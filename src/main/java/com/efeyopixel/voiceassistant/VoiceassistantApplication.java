6874ac1c76a5714c476ac1b9682071df54afefba
festus <festus@efeyopixel.com>
2022-05-17 10:38:41 -0500

    adding other files

---
+++ src/main/java/com/efeyopixel/voiceassistant/VoiceassistantApplication.java
@@ -0,0 +1,20 @@
+package com.efeyopixel.voiceassistant;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@OpenAPIDefinition(
+		servers = {
+				@Server(url = "https://d6a6-159-235-182-65.ngrok.io", description = "Default Server URL")
+		}
+)
+public class VoiceassistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoiceassistantApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/efeyopixel/voiceassistant/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.efeyopixel.voiceassistant.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Value("${spring.security.user.password}")
+    String securityPassword;
+
+    @Bean
+    public MapReactiveUserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password(securityPassword)
+                .roles("USER")
+                .build();
+        return new MapReactiveUserDetailsService(user);
+    }
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange(exchanges -> exchanges
+                        .anyExchange().authenticated()
+                )
+                .httpBasic(withDefaults())
+        .csrf().disable();
+//                .formLogin(withDefaults()); disable form login
+        return http.build();
+    }
+}
